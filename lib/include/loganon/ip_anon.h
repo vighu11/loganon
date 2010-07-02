019909e8c938827236e8a4366c8f0bb135d2bbd1
escovabr <escovabr@localhost>
2010-07-02 18:18:38 -0300

    Adding coherently anonymization, lists behaviors need some fixes

--- lib/include/loganon/ip_anon.h
+++ lib/include/loganon/ip_anon.h
@@ -15,7 +15,21 @@ char * random_permutation();
 struct ip_table * new_table();
 
 
+typedef struct node{
+	unsigned long int index;
+	unsigned long int field_value;
+	struct _node *prox;
 
-struct addr * ipv4_coherently_anon (struct addr ip);
+}_node;
+
+
+struct addr * ipv4_coherently_anon (struct addr ip, struct node *);
 struct addr * ipv4_black_marker (struct addr, int);
 struct addr * ipv4_field_rotation (struct addr, int);
+
+
+unsigned long int new_unique_ip(struct node * head);
+
+
+struct node * new_ip_list ();
+void put_on_top(struct node * head, struct node * current, struct node * last);
