831ca4859c53511ce9acf6a53dab63e2283324e7
escovabr <escovabr@localhost>
2010-07-05 10:30:36 -0300

    Repushing again

--- lib/random.c
+++ lib/random.c
@@ -1,4 +1,6 @@
 #include <loganon/random.h>
+#include <time.h>
+#include <stdlib.h>
 
 /* An algorithm to generate a random symetric key */
 void loganon_random_ultraweak_symkey(char *s, const int len) {
@@ -15,4 +17,24 @@ void loganon_random_ultraweak_symkey(char *s, const int len) {
 	s[len] = 0;
 }
 
+unsigned long int loganon_random_ip(){
 
+	int r;
+	int num;
+	time_t now = time(NULL);
+	if (now == (time_t) -1) {
+		  /* handle error */
+	  }
+	 srandom(now); 
+
+
+	unsigned long int field1=0, field2=0,field3=0,field4=0;
+	//field1 = (random() % 254);
+	//field2 = (random() % 254 )<< 8;
+	//field3 = (random() % 254 )<< 16;
+	field4 = (random() % 4294967295 );
+    /* generate secret number: */
+	return field4;    	
+
+
+}
