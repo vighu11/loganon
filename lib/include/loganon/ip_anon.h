0319ef55ab15cb61f73bb0c53887f549a9f3f267
escovabr <escovabr@localhost>
2010-07-05 09:36:49 -0300

    Repushing, add uthash and new ip_anon stuff

--- lib/include/loganon/ip_anon.h
+++ lib/include/loganon/ip_anon.h
@@ -5,7 +5,7 @@
 #include <dumbnet.h>
 //nclude <math.h>
 #include <openssl/lhash.h>
-
+#include <uthash/uthash.h>
 
 char * truncation ( char *, int);
 char * black_marker(char *, int );
@@ -33,3 +33,22 @@ unsigned long int new_unique_ip(struct node * head);
 
 struct node * new_ip_list ();
 void put_on_top(struct node * head, struct node * current, struct node * last);
+
+
+
+/* Structures for UTHASH LIBRARY */
+struct ip_node {
+	unsigned long int index;
+	unsigned long int newValue;
+	UT_hash_handle hh;
+
+};
+
+struct ip_node *create_hash_table();
+int add_to_hash(unsigned long int key, unsigned long int newValue);
+struct ip_node *create_a_node(unsigned long int key, unsigned long int newValue);
+
+unsigned long int using_hash_anon(struct ip_node *hash_table, unsigned long int old_ip);
+
+
+
