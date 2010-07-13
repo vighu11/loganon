748e59d74f4c8cf8c5aa7c55183acef66c4b46ea
taron <taron@dell-desktop.example.com>
2010-07-13 15:26:11 +0200

    Add loganon_regex.h

---
+++ lib/parser/loganon_regex.h
@@ -0,0 +1,17 @@
+/*
+ *	Regex definitions
+ *	Authors: Guillaume TOURON
+ */
+
+#ifndef LOGANON_REGEX_H
+#define LOGANON_REGEX_H
+
+/* Size for results array */
+#define OVECCOUNT 3
+
+/*
+ * Regular expressions for searching sensitive data
+ */
+#define IP_REGEX "[0-9]a"
+
+#endif
