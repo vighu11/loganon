63716544d400dec9587bdf86a122f0d674af84bd
escovabr <escovabr@localhost>
2010-07-23 14:37:48 -0300

    add some string anonymization files, updade Cmakelists, add stranon test exe

---
+++ lib/include/loganon/str_anon.h
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dumbnet.h>
+#include <uthash/uthash.h>
+
+
+void loganon_str_test_all(char *);
+
+char * loganon_md5_digest ( const void* text, int len);
+
+char * return_as_hex(const unsigned char *, int);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
