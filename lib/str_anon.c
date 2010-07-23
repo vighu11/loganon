4239c8b44487b3d59c78b5100899da9cc3ad5461
escovabr <escovabr@localhost>
2010-07-23 15:22:19 -0300

    Free strings in str_anon, resolve some memory leaks problems

--- lib/str_anon.c
+++ lib/str_anon.c
@@ -6,10 +6,12 @@
 
 void loganon_str_test_all(char *arg){
 	//A Function to test all methods of string anonymization
+	char * md5value = NULL;
+	
 	printf("%s\n",arg);
-	char * md5value = loganon_md5_digest(arg, strlen(arg)*sizeof(char));
-	printf("\tMD5SUM = %s \n", md5value);
-	//free(md5value);
+	md5value = (char *) loganon_md5_digest(arg, strlen(arg)*sizeof(char));
+	printf("\tMD5SUM = %s \n",(char *)  md5value);
+	free(md5value);
 
 }
 
@@ -18,7 +20,7 @@ void loganon_str_test_all(char *arg){
 char * loganon_md5_digest (const void * text, int len){
 
 	EVP_MD_CTX holder;
-	unsigned char mdvalue[EVP_MAX_MD_SIZE], *temp;
+	unsigned char mdvalue[EVP_MAX_MD_SIZE], *temp=NULL;
 	unsigned int mdlen;
 
 	EVP_DigestInit(&holder, EVP_md5());
@@ -28,7 +30,7 @@ char * loganon_md5_digest (const void * text, int len){
 	EVP_DigestFinal_ex(&holder, mdvalue, &mdlen);
 
 
-	temp = return_as_hex(mdvalue,mdlen);
+	temp = (char *) return_as_hex(mdvalue,mdlen);
 
 
 	EVP_MD_CTX_cleanup(&holder);
@@ -42,13 +44,12 @@ char * loganon_md5_digest (const void * text, int len){
 
 char * return_as_hex (const unsigned char *digest, int len) {
   int i;
-  char * hex = (char *) calloc(len+1,sizeof(char) );
-  hex[0]='\0';
-  char buffer[10];
+  char *hex,buffer[EVP_MAX_MD_SIZE]="\0",current[2];
     for(i = 0; i < len; i++){
-       		 sprintf (buffer,"%02x", digest[i]);
-		 strcat(hex,buffer);
+       		 sprintf (current,"%02x", digest[i]);
+		 strcat(buffer,current);
 		 }
+   hex = strdup(buffer);
    return hex;
 }
 
