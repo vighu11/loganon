1bd38d6b0dc2e6f12b8bcbbaa1d6744ed3ee039a
escovabr <escovabr@localhost>
2010-08-07 21:16:51 -0300

    Add sha1 digest, and doxygen headers

--- lib/include/loganon/str_anon.h
+++ lib/include/loganon/str_anon.h
@@ -10,6 +10,8 @@ void loganon_str_test_all(char *);
 
 char * loganon_md5_digest ( const void* text, int len);
 
+char * loganon_sha1_digest ( const void* text, int len);
+
 char * return_as_hex(const unsigned char *, int);
 
 
